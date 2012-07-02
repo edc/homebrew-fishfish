3a88304e5f108e4528de6c76087fc2b90db64ba8
Eddie Cao <cao.yiqun@gmail.com>
2012-07-02 09:40:49 -0700

    add a link to the homepage

--- Formula/fishfish.rb
+++ Formula/fishfish.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Fishfish < Formula
-  homepage ''
+  homepage 'http://ridiculousfish.com/shell/'
   head 'git://github.com/fish-shell/fish-shell.git'
   url 'http://ridiculousfish.com/shell/files/fishfish.tar.gz'
   md5 'ebe5fe17f6925b9142aadc8ebae5fba1'
