e2cf8f993b6ef12fdbd1705148bb5451defa0c81
Eddie Cao <cao.yiqun@gmail.com>
2012-07-02 09:40:16 -0700

    now points to github instead of gitorious

--- Formula/fishfish.rb
+++ Formula/fishfish.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Fishfish < Formula
   homepage ''
-  head 'git://gitorious.org/~ridiculousfish/fish-shell/fishfish.git'
+  head 'git://github.com/fish-shell/fish-shell.git'
   url 'http://ridiculousfish.com/shell/files/fishfish.tar.gz'
   md5 'ebe5fe17f6925b9142aadc8ebae5fba1'
   version '1.23.1'
